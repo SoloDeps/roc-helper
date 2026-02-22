49a221a1b3c9952161b3c2c437e41ba5bda428ee
SoloDeps <solodevops31@gmail.com>
2026-02-22 13:11:29 +0100

    refactor: update next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  distDir: "dist",
   reactCompiler: true,
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
